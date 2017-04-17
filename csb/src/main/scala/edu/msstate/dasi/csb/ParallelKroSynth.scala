e455c58dde8dfce154a858eed59f4f5c1d681706
Stefano Cordio <scordio@users.noreply.github.com>
2017-04-17 15:22:41 -0500

    Replace the current implementation with a revised version of the other Kronecker algorithm: the previous parallel Kronecker had exponential complexity, while the new one should have linear complexity.

--- csb/src/main/scala/edu/msstate/dasi/csb/ParallelKroSynth.scala
+++ csb/src/main/scala/edu/msstate/dasi/csb/ParallelKroSynth.scala
@@ -23,38 +23,80 @@ class ParallelKroSynth(partitions: Int, mtxFile: String, iterations: Int) extend
    * The KronFit algorithm is a gradient descent based algorithm which ensures that the probability of generating the
    * original graph from the small probability matrix after performing Kronecker multiplications is very high.
    */
-  private def kronFit(seed: Graph[VertexData, EdgeData]): Array[(Long, Long, Double)] = {
+  private def kronFit(seed: Graph[VertexData, EdgeData]): Array[(VertexId, VertexId, Double)] = {
     loadMtx(mtxFile)
   }
 
   /**
    * Generates a graph using a parallel implementation of the stochastic Kronecker algorithm.
    */
-  private def stochasticKro(seedMtx: Array[(VertexId, VertexId, Double)], seedDists: DataDistributions): Graph[VertexData,EdgeData] = {
-    val seedSize = seedMtx.length
+  private def stochasticKro(seedMtx: Array[(VertexId, VertexId, Double)], seedDists: DataDistributions, iterations: Int): Graph[VertexData,EdgeData] = {
+    val seedVerticesNum = seedMtx.length / 2
+    val probSum = seedMtx.map{ case (_, _, prob) => prob}.sum
 
-    var rawEdges = sc.parallelize(
-      seedMtx.flatMap{ case (src, dst, prob) =>
-        if (Random.nextDouble() < prob) {
-          Array((src, dst))
-        } else {
-          None
-        }
-      }
-    )
+    val expectedVertices = math.pow(seedVerticesNum, iterations).toLong
+    val expectedEdges = math.pow(probSum, iterations).toLong
 
-    for (_ <- 1 until iterations) {
-      rawEdges = rawEdges.flatMap{ case (srcId, dstId) =>
-        var edges = Array.empty[(VertexId, VertexId)]
+    println(s"Expected # of Vertices: $expectedVertices")
+    println(s"Expected # of Edges: $expectedEdges")
 
-        for ( (seedEdgeSrc, seedEdgeDst, seedEdgeProb) <- seedMtx ) {
-          if (Random.nextDouble() < seedEdgeProb) {
-            edges :+= (srcId * seedSize + seedEdgeSrc, dstId * seedSize + seedEdgeDst)
-          }
-        }
-        edges
-      }.repartition(partitions)
+    var cumulativeProb = 0.0
+    var cumulativeProbMtx = Array.empty[(VertexId, VertexId, Double)]
+
+    for ( (src, dst, prob) <- seedMtx ) {
+      cumulativeProb += prob
+      cumulativeProbMtx :+= (src, dst, cumulativeProb / probSum)
     }
+
+    var rawEdges = sc.emptyRDD[(VertexId, VertexId)]
+    var edgesCount = 0L
+
+    do {
+      val edgesPerPartition = (expectedEdges - edgesCount) / partitions
+      val remainingEdges = (expectedEdges - edgesCount) % partitions
+
+      val oldEdges = rawEdges
+
+      val emptySeq = sc.parallelize(Array.empty[(VertexId, VertexId)], partitions)
+
+      val newEdges = emptySeq.mapPartitionsWithIndex { (partitionIndex, _) =>
+        var count = 0L
+        var partitionEdges = Array.empty[(VertexId, VertexId)]
+
+        if (partitionIndex < remainingEdges) count -= 1
+
+        while (count < edgesPerPartition) {
+          var range = expectedVertices
+          var src = 0L
+          var dst = 0L
+
+          for (i <- 0 until iterations) {
+            var seedIndex = 0
+            val prob = Random.nextDouble()
+
+            while (prob > cumulativeProbMtx(seedIndex)._3) {
+              seedIndex += 1
+            }
+
+            range = range / seedVerticesNum
+            src += cumulativeProbMtx(seedIndex)._1 * range
+            dst += cumulativeProbMtx(seedIndex)._2 * range
+          }
+
+          partitionEdges :+= (src, dst)
+          count += 1
+        }
+
+        partitionEdges.iterator
+      }
+
+      rawEdges = rawEdges.union(newEdges).coalesce(partitions).distinct().persist(StorageLevel.MEMORY_AND_DISK)
+
+      edgesCount = rawEdges.count()
+
+      oldEdges.unpersist()
+    }
+    while (edgesCount < expectedEdges)
 
     val seedDistsBroadcast = sc.broadcast(seedDists)
 
@@ -84,6 +126,6 @@ class ParallelKroSynth(partitions: Int, mtxFile: String, iterations: Int) extend
     println(s"Running parallel Kronecker with $iterations iterations.")
     println()
 
-    stochasticKro(kronFit(seed), seedDists)
+    stochasticKro(kronFit(seed), seedDists, iterations)
   }
 }
