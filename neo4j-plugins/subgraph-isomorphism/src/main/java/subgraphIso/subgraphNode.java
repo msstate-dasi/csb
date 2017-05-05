440d4cda61b2e7323d36badc94e2d6dbadeddeda
Rui <jiatistuta@gmail.com>
2017-05-05 15:38:21 -0500

    add project files

---
+++ neo4j-plugins/subgraph-isomorphism/src/main/java/subgraphIso/subgraphNode.java
@@ -0,0 +1,12 @@
+package subgraphIso;
+import org.neo4j.graphdb.Node;
+
+
+public class subgraphNode {
+    public final Node node;//a node in the subgraph
+
+
+    public subgraphNode(Node node) {
+        this.node = node;
+    }
+}
