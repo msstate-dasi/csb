346c8bb68c291d4f8769cca9d5dcea189c5293e3
Stefano Cordio <scordio@users.noreply.github.com>
2017-05-09 14:37:03 -0500

    Remove scripts not related to the sources.

--- csb/env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-
-export SPARK_HOME=/work/$USER/spark
-export TZ=/usr/share/zoneinfo/Europe/Helsinki
--- csb/run.sh
+++
@@ -1,1 +0,0 @@
-$SPARK_HOME/bin/spark-submit --driver-memory=15G --executor-memory=10G csb-dep.jar ba seed_vert seed_edges 1000
