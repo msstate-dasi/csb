59f0544ba5754dc631e25a68bcdf068176299c07
Rui <jiatistuta@gmail.com>
2017-05-10 20:01:10 -0500

    Fix a bug: when the candidate list is sorted by the size, the result of subgraphs may be incomplete.

--- neo4j-plugins/subgraph-isomorphism/src/main/java/subgraphIso/Subgraph_Isomorphism.java
+++ neo4j-plugins/subgraph-isomorphism/src/main/java/subgraphIso/Subgraph_Isomorphism.java
@@ -4,12 +4,10 @@ import java.util.*;
 import java.util.stream.Stream;
 
 import org.neo4j.graphdb.*;
-import org.neo4j.graphdb.index.Index;
-import org.neo4j.graphdb.index.IndexManager;
 import org.neo4j.logging.Log;
 import org.neo4j.procedure.*;
 
-import static org.neo4j.helpers.collection.MapUtil.stringMap;
+
 
 /**
  * Subgraph Isomorphism
@@ -53,8 +51,6 @@ public class Subgraph_Isomorphism
             if (matchedSubgraphs.isEmpty())
                 return null;
             else {
-                    ArrayList<Node> resultNodes=new ArrayList<>();
-
                     for(int i=0;i<matchedSubgraphs.size();i++)
                     {
                         for(int j=0;j<matchedSubgraphs.get(i).size();j++)
@@ -83,33 +79,16 @@ public class Subgraph_Isomorphism
 
     private List<List<Node>> UllmannAlg(Label queryLabel, Label targetLabel,ArrayList<Node> candidateListIndex){
 
-        List<List<Node>> candidateList=findCandidates(queryLabel,targetLabel,candidateListIndex);//create the candidate list and index
+
         List<List<Node>> queryNeighborList=new ArrayList<>();// the neighbor list for query vertices
         List<List<Node>> nodeNeighborList=new ArrayList<>();// the neighbor list for the Neo4j database with the target label
         List<List<Node>> matchedSubgraphs=new ArrayList<>();//store the final results
 
 
-        //sort the candidate list by the number of candidates that a node has.
-        Comparator<List<Node>> candidateListSizeComparator=new Comparator<List<Node>>() {
-            @Override
-            public int compare(List<Node> o1, List<Node> o2) {
-                return o1.size()-o2.size();
-            }
-        };
-        candidateList.sort(candidateListSizeComparator);
-
-        //create the query graph's neighbor list
-        ResourceIterator<Node> queryNodes=db.findNodes(queryLabel);
-
-        while(queryNodes.hasNext())
-        {
-            Node queryNode=queryNodes.next();
-            queryNeighborList.add(findNodeNeighbors(queryNode));
-        }
-
-        queryNodes.close();
-
-        //create the neighbor list for the Neo4j database with the target label
+        /**
+         * Note: the sequence of the following three steps is strict.
+         **/
+        //Step 1: create the neighbor list for the Neo4j database with the target label
         ResourceIterator<Node> targetNodes;
         ArrayList<Node> nodeNeighborListIndex=new ArrayList<>();
         if(targetLabel.name().equals("All"))
@@ -125,11 +104,18 @@ public class Subgraph_Isomorphism
             if (targetLabel.name().equals("All") && targetNode.hasLabel(queryLabel))
                 continue;
 
-            nodeNeighborList.add(findNodeNeighbors(targetNode));
+            nodeNeighborList.add(findNodeNeighbors(targetNode,targetLabel));
             nodeNeighborListIndex.add(targetNode);
         }
 
         targetNodes.close();
+
+        //Step 2: create the candidate list and its index
+
+        List<List<Node>> candidateList=findCandidates(queryLabel,candidateListIndex,nodeNeighborList,nodeNeighborListIndex);
+
+        //Step 3: create the query graph's neighbor list
+        candidateListIndex.stream().forEach(node->queryNeighborList.add(findNodeNeighbors(node,queryLabel)));
 
 
         refineCandidate(candidateList,queryNeighborList,nodeNeighborList,candidateListIndex,nodeNeighborListIndex);//the first round refine
@@ -138,8 +124,6 @@ public class Subgraph_Isomorphism
             return matchedSubgraphs;
         }
         backtracking(0,candidateList,queryNeighborList,nodeNeighborList,candidateListIndex,nodeNeighborListIndex,matchedSubgraphs);
-
-
 
         return matchedSubgraphs;
 
@@ -152,10 +136,11 @@ public class Subgraph_Isomorphism
 
         if(numLayer==queryNeighborList.size())
         {
-            ArrayList<Node> subgraph=new ArrayList<>();
+            ArrayList<Node> subgraph=new ArrayList<>();//each subgraph contains a matching isomorphic subgraph
 
             for(int i=0;i<candidateList.size();i++)
             {
+                //at this recursion layer, each query node has exactly one candidate node.
                 subgraph.add(candidateList.get(i).get(0));
 
             }
@@ -288,59 +273,65 @@ public class Subgraph_Isomorphism
     }
 
 
-    private List<List<Node>> findCandidates(Label queryLabel, Label targetLabel, ArrayList<Node> candidateListIndex)
+    private List<List<Node>> findCandidates(Label queryLabel, ArrayList<Node> candidateListIndex,List<List<Node>> nodeNeighborList, ArrayList<Node> nodeNeighborListIndex)
     {//find all query vertex candidates in the Neo4j database under the label "targetLabel"
         //input an empty candidate-list index list for modification
         List<List<Node>> candidateList=new ArrayList<>();
 
 
         ResourceIterator<Node> queryNodes=db.findNodes(queryLabel);
-        ResourceIterator<Node> targetNodes;
 
-        while(queryNodes.hasNext())
-        {
-            ArrayList<Node> candidateListPerVertex = new ArrayList<>();
-            Node queryNode=queryNodes.next();
-
-            if (targetLabel.name().equals("All"))
-                targetNodes = db.getAllNodes().iterator();
-            else
-                targetNodes=db.findNodes(targetLabel);
-
-            while(targetNodes.hasNext())
+            while(queryNodes.hasNext())
             {
-                Node targetNode=targetNodes.next();
+                ArrayList<Node> candidateListPerVertex = new ArrayList<>();
+                Node queryNode=queryNodes.next();
+                int queryNodeDegree=findNodeNeighbors(queryNode,queryLabel).size();
 
-                //pass the nodes in the query graph
-                if (targetLabel.name().equals("All") && targetNode.hasLabel(queryLabel))
-                    continue;
-
-                if(targetNode.getDegree()>=queryNode.getDegree())
-                {
-                    candidateListPerVertex.add(targetNode);
-                }
+               for(int i=0;i<nodeNeighborList.size();i++)
+               {
+                   int targetNodeDegree=nodeNeighborList.get(i).size();
+                   if(targetNodeDegree>=queryNodeDegree)
+                       candidateListPerVertex.add(nodeNeighborListIndex.get(i));
+               }
+                candidateListPerVertex.add(0,queryNode);// temporarily append the query node to the first position of the list
+                candidateList.add(candidateListPerVertex);
             }
-            candidateListIndex.add(queryNode);
-            candidateList.add(candidateListPerVertex);
-            targetNodes.close();
 
-        }
+            //sort the candidate list by the number of candidates (ascending)
+            Comparator<List<Node>> candidateListSizeComparator =new Comparator<List<Node>>() {
+                @Override
+                public int compare(List<Node> o1, List<Node> o2) {
+
+                    return o1.size()-o2.size();
+                }
+            };
+            candidateList.sort(candidateListSizeComparator);
+
+        candidateList.stream().forEach(nodes -> candidateListIndex.add(nodes.get(0)));
+        candidateList.stream().forEach(nodes -> nodes.remove(0));//remove the temorary query node at position 0
+
         queryNodes.close();
-
-
         return candidateList;
     }
 
-    private ArrayList<Node> findNodeNeighbors(Node node)
+    private ArrayList<Node> findNodeNeighbors(Node node, Label targetLabel)
     {//find the neighbors of a node in the Neo4j database
         ArrayList<Node> neighborsOfnode=new ArrayList<>();
         Iterator<Relationship> relationships=node.getRelationships().iterator();
         while(relationships.hasNext()){
-            neighborsOfnode.add(relationships.next().getOtherNode(node));
+            Node neighborNode=relationships.next().getOtherNode(node);
+            if (targetLabel.name().equals("All"))
+            {
+                neighborsOfnode.add(neighborNode);
+            }
+            else if (neighborNode.hasLabel(targetLabel))
+            {
+                neighborsOfnode.add(neighborNode);// only add nodes that have the target label and ignore others
+            }
+
         }
         return neighborsOfnode;
     }
-
 
 
 }
